ef5d5ca37550b42d6485ddfb89d413157ee607af
imre-gabor <gabor@aut.bme.hu>
2024-02-01 19:28:05 +0100

    use public/private keypair in JwtService

--- microservices/token-lib/src/main/java/hu/webuni/tokenlib/JwtService.java
+++ microservices/token-lib/src/main/java/hu/webuni/tokenlib/JwtService.java
@@ -1,10 +1,13 @@
 package hu.webuni.tokenlib;
 
 
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -15,12 +18,33 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
+import jakarta.annotation.PostConstruct;
+
 @Service
 public class JwtService {
 
     private static final String AUTH = "auth";
-    private Algorithm alg = Algorithm.HMAC256("mysecret");
     private String issuer = "webuni-user-service";
+
+    @Value("${hu.webuni.tokenlib.keypaths.private:#{null}}")
+    private String pathToPemWithPrivateKey;
+    @Value("${hu.webuni.tokenlib.keypaths.public:#{null}}")
+    private String pathToPemWithPublicKey;
+    private Algorithm signerAlg;
+    private Algorithm validatorAlg;
+    
+    
+    
+    @PostConstruct
+    public void init() throws Exception {
+    	if(pathToPemWithPrivateKey != null) {
+    		signerAlg = Algorithm.ECDSA512(null, (ECPrivateKey)PemUtils.getPrivateKey(pathToPemWithPrivateKey));
+    	}
+    	
+    	if(pathToPemWithPublicKey != null) {
+    		validatorAlg = Algorithm.ECDSA512((ECPublicKey)PemUtils.getPublicKey(pathToPemWithPublicKey), null);
+    	}
+    }
     
     public String creatJwtToken(UserDetails principal) {
         return JWT.create()
@@ -28,13 +52,13 @@ public class JwtService {
             .withArrayClaim(AUTH, principal.getAuthorities().stream().map(GrantedAuthority::getAuthority).toArray(String[]::new))
             .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(20)))
             .withIssuer(issuer)
-            .sign(alg);
+            .sign(signerAlg);
         
     }
 
     public UserDetails parseJwt(String jwtToken) {
         
-        DecodedJWT decodedJwt = JWT.require(alg)
+        DecodedJWT decodedJwt = JWT.require(validatorAlg)
             .withIssuer(issuer)
             .build()
             .verify(jwtToken);
