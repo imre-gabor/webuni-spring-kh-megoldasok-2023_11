59f4e2f402de221eb1744251787b11bcb257899c
imre-gabor <gabor@aut.bme.hu>
2024-02-01 19:39:59 +0100

    implement JWT check at gateway

--- microservices/gateway/src/main/java/hu/webuni/gateway/GatewayApplication.java
+++ microservices/gateway/src/main/java/hu/webuni/gateway/GatewayApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+import hu.webuni.tokenlib.JwtAuthFilter;
+
+@SpringBootApplication(scanBasePackageClasses = {JwtAuthFilter.class, GatewayApplication.class})
 @EnableDiscoveryClient
 public class GatewayApplication {
 
